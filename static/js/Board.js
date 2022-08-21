b658dd1e08e6facd2f3436766e88493d26fd5fcb
Liske1306 <58723563+Liske1306@users.noreply.github.com>
2022-08-21 21:21:16 +0200

    gigacrusc(v1.0.1)
    aaaaaa

--- static/js/Board.js
+++ static/js/Board.js
@@ -19,5 +19,83 @@ class Board {
             box.style.backgroundColor = backgroundColor;
             $("#board").append(box);
         }
+<<<<<<< Updated upstream
+=======
+        $(document).ready(function () {
+            fish.PlaceFishes();
+            rook.PlaceRooks();
+            monke.PlaceMonkeys();
+            elephant.PlaceElephants();
+            king.PlaceKings();
+            queen.PlaceQueens();
+            board.PlacePawns();
+        });
+    }
+
+    PlacePawns() {
+        for (var i in board.PawnList) {
+            if (!board.PawnList[i].captured) {
+                board.CreatePawn(board.PawnList[i]);
+            }
+        }
+    }
+
+    CreatePawn(PawnInfo) {
+        var pawn = document.createElement("img");
+        pawn.className = PawnInfo.type;
+        pawn.src = "img/" + PawnInfo.color + "/" + PawnInfo.type + ".png";
+        pawn.onclick = function () {
+            board.PawnFunction(this);
+        };
+        $("#Box" + PawnInfo.position).append(pawn);
+    }
+
+    BoardReload() {
+        //PlacePawnsFromPawnList
+        let boardDiv = $(".BoardField");
+        for (var i = 0; i < boardDiv.length; i++) {
+            $("#" + boardDiv[i].id).empty();
+        }
+        this.PlacePawns();
+    }
+
+    PawnFunction(DIV) {
+        if (!board.imprisoning) {
+            try {
+                $(".selected")[0].classList.remove("selected");
+            } catch { }
+            DIV.classList.add("selected");
+            board.Highlighted.forEach((element) => {
+                var el = element.id.slice(3, 5);
+                console.log(el)
+                document.getElementById("Box" + el).classList.remove("highlighted");
+                if (DIV.parentElement && DIV.parentElement == element) board.Capture(DIV.parentElement);
+            });
+            board.Highlighted = [];
+            for (var j in board.PawnList) {
+                try {
+                    if (
+                        !board.PawnList[j].captured &&
+                        board.PawnList[j].position == DIV.parentElement.id.slice(3, 5) &&
+                        (ui.CurrentPawn == undefined || ui.CurrentPawn.color == DIV.src.split("/")[DIV.src.split("/").length - 2])
+                    )
+                        board.PawnList[j].movement();
+                } catch { }
+            }
+        }
+    }
+
+    Capture(Pawn) {
+        board.capturing = true;
+        $("#Box" + ui.CurrentPawn.position)[0].children[0].classList.add("selected");
+        for (var j in board.PawnList)
+            if (board.PawnList[j].position == Pawn.id.slice(3, 5)) {
+                if (board.PawnList[j].type == "king" || board.PawnList[j].type == "queen") {
+                    board.imprisoning = true;
+                    board.PawnList[j].imprisonment();
+                } else board.PawnList[j].captured = true;
+            }
+        ui.move(Pawn);
+>>>>>>> Stashed changes
     }
 }
--- static/js/pawns/Monke.js
+++ static/js/pawns/Monke.js
@@ -1,8 +1,18 @@
+<<<<<<< Updated upstream
 console.log("wczytano plik Monke.js");
 class Monke {
     constructor() {
         console.log("konstruktor klasy Monke");
         this.PlaceMonkes();
+=======
+//console.log("wczytano plik Monke.js");
+class Monke extends Pawn {
+    constructor(position, type, color) {
+        //console.log("konstruktor klasy Monke");
+        super(position, type, color);
+        this.jump = []
+        this.howmanymonke = 1
+>>>>>>> Stashed changes
     }
     PlaceMonkes() {
         var Monkes = [1, 6, 57, 62];
@@ -17,5 +27,74 @@ class Monke {
             };
             $("#Box" + Monkes[i]).append(monke);
         }
+<<<<<<< Updated upstream
+=======
+        var Monkeys = board.PawnsPositions.Black.Monkeys;
+        for (var i in Monkeys.position) {
+            board.PawnList.push(
+                new Monke(Monkeys.position[i], Monkeys.name, "Black")
+            );
+        }
+    }
+    movement() {
+        board.Highlighted = []
+        monke.jump = []
+        monke.howmanymonke = 1
+        ui.CurrentPawn = this
+        monke.jump.push(this.position)
+        for (let monketab = 0; monketab <= monke.howmanymonke; monketab++) {
+            this.monkes(monke.jump[monketab])
+        }
+        for (let col = -1; col <= 1; col++) {
+            for (let row = -1; row <= 1; row++) {
+                try {
+                    if (col == 0 || row == 0) {
+                        if (col == 0 && row == 0) { }
+                        else {
+                            let tempdiv = $("#Box" + String.fromCharCode(ui.CurrentPawn.position.charCodeAt(0) + col) + (parseInt(ui.CurrentPawn.position.substr(1)) + row))[0]
+                            if (tempdiv.children.length == 0) {
+                                tempdiv.classList.add("highlighted");
+                                board.Highlighted.push(tempdiv);
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+
+    }
+    monkes(pos) {
+        for (let col = -1; col <= 1; col++) {
+            for (let row = -1; row <= 1; row++) {
+                try {
+                    if (row != 0 || col != 0) {
+                        //console.log(pos)
+                        if (($("#Box" + String.fromCharCode(pos.charCodeAt(0) + col) + (parseInt(pos.substr(1)) + row))[0].children[0])) {
+                            let tempdiv = $("#Box" + String.fromCharCode(pos.charCodeAt(0) + col * 2) + (parseInt(pos.substr(1)) + row * 2))[0]
+                            if (tempdiv.childElementCount == 0) {
+                                tempdiv.classList.add("highlighted");
+                                board.Highlighted.push(tempdiv);
+                                if (!monke.jump.includes(String.fromCharCode(pos.charCodeAt(0) + col * 2) + (parseInt(pos.substr(1)) + row * 2))) {
+                                    monke.jump.push(String.fromCharCode(pos.charCodeAt(0) + col * 2) + (parseInt(pos.substr(1)) + row * 2))
+                                    monke.howmanymonke++
+                                }
+                            } else {
+                                board.PawnList.forEach((element) => {
+                                    if (String.fromCharCode(pos.charCodeAt(0) + col * 2) + (parseInt(pos.substr(1)) + row * 2) == element.position) {
+                                        if (element.color != this.color) {
+                                            tempdiv.classList.add("highlighted");
+                                            board.Highlighted.push(tempdiv);
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    }
+                } catch { }
+            }
+        }
+        //console.log(monke.jump)
+>>>>>>> Stashed changes
     }
 }
